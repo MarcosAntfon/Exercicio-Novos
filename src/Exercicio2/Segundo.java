295d14a947a009375b6aba388cdc0ba6041d8728
Marcos Santos <89051454+MarcosAntfon@users.noreply.github.com>
2021-09-06 15:43:04 -0300

    Create Segundo.java

---
+++ src/Exercicio2/Segundo.java
@@ -0,0 +1,24 @@
+package Exercicio2;
+
+import java.util.Scanner; 
+
+public class Segundo {
+
+	public static void main(String args[]) {          
+		
+		Scanner input = new Scanner(System.in);    
+		
+		double C, F;          
+		
+		System.out.print("Digite a temperatura em Graus Celsius:" );          
+		 
+		C = input.nextDouble();          
+		
+		F = ( 9 * C + 160 ) / 5;  
+		
+System.out.printf(" A temperatura convertida para Fahrenheit é:" +F);       
+	
+	}  
+}
+
+
